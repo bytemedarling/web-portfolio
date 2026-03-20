d2718e2f3b037f31f207ad578235be4e371659eb
bytemedarling <jessicakostyk@outlook.com>
2026-03-20 14:53:50 -0400

    added new hamburger button and nav styling; added logo image

---
+++ script.js
@@ -0,0 +1,10 @@
+const menuButton = document.querySelector('.menu-button');
+const navMenu = document.querySelector('.main-nav');
+
+menuButton.addEventListener('click', () => {
+    const isOpen = navMenu.style.display === 'block';
+
+    navMenu.style.display = isOpen ? 'none' : 'block';
+    menuButton.classList.toggle('open');
+    menuButton.setAttribute('aria-expanded', !isOpen);
+});
