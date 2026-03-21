b047abac1d1bcda6e9a7fa3de3337b3ccfd4ab95
bytemedarling <jessicakostyk@outlook.com>
2026-03-21 13:47:05 -0400

    added background to send button

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* Button Toggle for Mobile Navigation */
 const menuButton = document.querySelector('.menu-button');
 const navMenu = document.querySelector('.main-nav');
 
@@ -9,4 +10,15 @@ menuButton.addEventListener('click', () => {
     menuButton.setAttribute('aria-expanded', !isOpen);
 });
 
+/* Project Card Effeect */
+const cards = document.querySelectorAll('.project-card');
 
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+});
+
+cards.forEach(card => observer.observe(card));
